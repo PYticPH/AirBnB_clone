6a39214a185731fe19eace26e4ddd251a3825ac3
PYticPH <pyticph@gmail.com>
2023-02-09 03:10:56 -0800

    change value of updated_at to type datetime

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = None
+        self.updated_at = datetime.datetime.now()
 
     def save(self):
         """ Update UUID creation time """
